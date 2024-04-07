28073336feac0c4a0910de9699c09251fc40d1cf
Nordup <nordup.ondr@gmail.com>
2024-04-07 16:36:27 +0400

    music

--- levels/portal/portal.gd
+++ levels/portal/portal.gd
@@ -5,12 +5,18 @@ extends Node3D
 
 func _on_portal_entered(body):
 	if not body is Player: return
+	play_enter_audio()
+	
 	if body.get_multiplayer_authority() != multiplayer.get_unique_id(): return
 	
 	print("Portal_entered: " + url)
-	await get_tree().create_timer(0.2).timeout
+	await get_tree().create_timer(0.3).timeout
 	
 	if get_tree().has_method("open_gate"):
 		get_tree().open_gate(url)
 	else:
 		push_warning("Tree doesn't have method open_gate. Do nothing")
+
+
+func play_enter_audio():
+	$AudioStreamPlayer3D.play()
