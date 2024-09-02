d97ce4e54e3346764abf1e36e0e837b24f2fefe8
Nordup <nordup.ondr@gmail.com>
2024-09-03 01:39:05 +0400

    interactable sound effect

--- levels/interactable/interactable.gd
+++ levels/interactable/interactable.gd
@@ -2,6 +2,7 @@ extends Node3D
 
 @export var area: Area3D
 @export var animation: InteractableAnimation
+@export var sound: AudioStreamPlayer3D
 
 var entered_count: int
 
@@ -16,6 +17,8 @@ func body_entered(body: Node3D) -> void:
 	
 	if entered_count == 0: animation.play_activate()
 	entered_count+=1
+	
+	sound.play()
 
 
 func body_exited(body: Node3D) -> void:
