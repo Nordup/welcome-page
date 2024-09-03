dfad0a282736e00e465e6b7ba70003607f6d775a
Nordup <nordup.ondr@gmail.com>
2024-09-04 00:28:59 +0400

    tree send_command

--- levels/interactable/interactable_link.gd
+++ levels/interactable/interactable_link.gd
@@ -4,4 +4,7 @@ extends InteractableBase
 
 
 func interact() -> void:
-	OS.shell_open(link)
+	if get_tree().has_method("send_command"):
+		get_tree().send_command("open_link", [link])
+	else:
+		OS.shell_open(link)
--- levels/portal/portal.gd
+++ levels/portal/portal.gd
@@ -12,10 +12,10 @@ func _on_portal_entered(body):
 	print("Portal_entered: " + url)
 	await get_tree().create_timer(0.3).timeout
 	
-	if get_tree().has_method("open_gate"):
-		get_tree().open_gate(url)
+	if get_tree().has_method("send_command"):
+		get_tree().send_command("open_link", [url])
 	else:
-		push_warning("Tree doesn't have method open_gate. Do nothing")
+		push_warning("Tree doesn't have method send_command. Do nothing")
 
 
 func play_enter_audio():
