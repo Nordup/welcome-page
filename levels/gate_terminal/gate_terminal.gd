f96d305047ac45a85ab62e13a3525a22a364b75c
Nordup <nordup.ondr@gmail.com>
2024-10-07 06:14:36 +0400

    better result ui, place in level

--- levels/gate_terminal/gate_terminal.gd
+++ levels/gate_terminal/gate_terminal.gd
@@ -1,5 +1,6 @@
 extends Node3D
 
+@export var start_url: String
 @export var interactable: InteractableTerminal
 @export var terminal_info: TerminalInfo
 @export var stand: StandBase
@@ -8,10 +9,12 @@ extends Node3D
 
 func _ready() -> void:
 	interactable.on_load_gate.connect(on_load_gate)
+	on_load_gate(start_url)
 
 
 func on_load_gate(url: String) -> void:
 	print("Loading gate: " + url)
+	if url.is_empty(): return
 	
 	terminal_info.set_info(url)
 	var success = await terminal_info.on_info_set
