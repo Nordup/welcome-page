13376ab3902b915fc77e5920ac59e9c645c5b048
Nordup <nordup.ondr@gmail.com>
2025-06-01 22:58:06 +0700

    fix portal entrence and spawn positions

--- levels/portal/portal.gd
+++ levels/portal/portal.gd
@@ -2,14 +2,16 @@ extends Node3D
 class_name Portal
 
 @export var url: String
+@onready var network_timeout: float = ProjectSettings.get("network/limits/tcp/connect_timeout_seconds")
 
 
-func _on_portal_entered(body):
+func _on_portal_entered(body: Node3D):
 	if not body is Player: return
 	play_enter_audio()
 	
-	if body.get_multiplayer_authority() != multiplayer.get_unique_id() and \
-		Connection.is_peer_connected: return
+	if body.get_multiplayer_authority() != multiplayer.get_unique_id() and Connection.is_peer_connected:
+		hide_client_body(body)
+		return
 	
 	Debug.log_msg("Portal_entered: " + url)
 	await get_tree().create_timer(0.3).timeout
@@ -22,3 +24,13 @@ func _on_portal_entered(body):
 
 func play_enter_audio():
 	$AudioStreamPlayer3D.play()
+
+
+func hide_client_body(body: Node3D):
+	await get_tree().create_timer(0.05).timeout
+	body.visible = false
+	
+	# In case of interpolation error, show body if server hasn't kicked out the client
+	await get_tree().create_timer(network_timeout + 1.0).timeout
+	if is_instance_valid(body):
+		body.visible = true
